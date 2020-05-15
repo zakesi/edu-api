650e9d82e9f9235e887fec148e438fdc2db2bae9
Jax Chu <294242589@qq.com>
2020-05-15 21:26:31 +0800

    feat: setting api

--- app/controller/user.js
+++ app/controller/user.js
@@ -1,12 +1,22 @@
 'use strict';
 
 const Controller = require('egg').Controller;
+const smsRule = {
+  phone: { type: 'string', required: true, message: '请输入手机号' },
+  code: { type: 'string', required: true, message: '请输入验证码' },
+};
 
 class userController extends Controller {
   async userInfo() {
     const { ctx } = this;
     const id = ctx.locals.user_id;
-    const userInfo = await ctx.model.User.findByPk(id);
+    const userInfo = await ctx.model.User.findByPk(id, {
+      attributes: [
+        'id', 'phone', 'name', 'real_name', 
+        'sex', 'birthday', 'introduction',
+        'avatar_url', 'unionid',
+      ]
+    });
     ctx.body = { error_code: 0, data: { userInfo }, message: 'success' };
   }
   async updateUserInfo() {
@@ -21,18 +31,55 @@ class userController extends Controller {
     });
     ctx.body = { error_code: 0, message: 'success' };
   }
-  async unBindPhone() {
-
-  }
   async bindPhone() {
-    // const { ctx } = this;
-    // const id = ctx.locals.user_id;
+    const { ctx } = this;
+    const id = ctx.locals.user_id;
+    const { phone, code } = ctx.request.body;
+    ctx.validate(smsRule, ctx.request.body);
+    const verify = await ctx.service.sms.verify(phone, code);
+    if(verify.error_code) {
+      return ctx.body = verify
+    }
+    await ctx.model.User.update({
+      phone: phone
+    },{
+      where: { id }
+    });
+    ctx.body = { error_code: 0, message: 'success' };
   }
   async bindWechat() {
+    const { ctx } = this;
+    const id = ctx.locals.user_id;
+    const code = ctx.query.code;
+    if(!code) {
+      return ctx.body = { error_code: 1, message: 'code necessary'}
+    }
+    const userInfo = await ctx.service.wechat.oAuthWeb(code);
+    const unionid = userInfo.unionid;
+    const hasUser = await ctx.model.User.findOne({
+      where: { unionid }
+    })
+    if(hasUser) {
+      return ctx.body = { error_code: 1, message: '该微信号已绑定，请解绑后再进行绑定'}
+    }
 
+    await ctx.model.User.update({
+      unionid: unionid
+    }, {
+      where: { id }
+    })
+
+    ctx.body = { error_code: 0, data: { unionid }, message: 'success' }
   }
   async unBindWechat() {
-    
+    const { ctx } = this;
+    const id = ctx.locals.user_id;
+    await ctx.model.User.update({
+      unionid: ''
+    }, {
+      where: { id }
+    })
+    ctx.body = { error_code: 0, message: 'success' }
   }
 }
 
--- app/router/index.js
+++ app/router/index.js
@@ -1,7 +1,8 @@
 'use strict';
 
 module.exports = app => {
-  const { router, controller } = app;
+  const { router, controller, middleware } = app;
+  const Auth = middleware.auth;
   router.get('/', controller.home.index);
   // 登录
   router.post('/api/sms/send', controller.auth.sms);
@@ -12,4 +13,10 @@ module.exports = app => {
   // 服务
   router.get('/api/qiniu/token', controller.qiniu.token);
   router.post('/api/notification/feishu', controller.notification.feishu);
+  // 用户
+  router.get('/api/user/user-info', Auth(), controller.user.userInfo);
+  router.put('/api/user/user-info', Auth(), controller.user.updateUserInfo);
+  router.put('/api/user/phone-bind', Auth(), controller.user.bindPhone);
+  router.get('/api/user/wechat-bind', Auth(), controller.user.bindWechat);
+  router.delete('/api/user/wechat-bind', Auth(), controller.user.unBindWechat);
 };
